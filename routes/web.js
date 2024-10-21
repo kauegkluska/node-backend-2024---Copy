79bf9ec5b09fd8e8f2f12345e79fe6a6c5fee835
leobravoe <leonardo.bravo.estacio@gmail.com>
2024-10-21 09:21:19 -0300

    Aula 21, 22, 23 e 24

--- routes/web.js
+++ routes/web.js
@@ -11,7 +11,11 @@ router.get("/recurso", (request, response) => {
 });
 
 router.get("/produto", async (request, response) => {
-    const produtos = await DataBase.executeSQLQuery("SELECT * FROM Produto");
+    const produtos = await DataBase.executeSQLQuery(`SELECT Produto.*,
+                                                            TipoProduto.descricao as TipoProduto_descricao
+                                                    FROM Produto
+                                                    JOIN TipoProduto ON Produto.TipoProduto_id = TipoProduto.id`);
+    //response.send({ produtos: produtos });
     response.render("Produto/index", { produtos: produtos });
 });
 
