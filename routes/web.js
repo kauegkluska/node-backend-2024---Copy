768a53e190e9cc888f5b154c8a7147b3942a2e70
Leonardo Bravo Estacio <leonardo.bravo@lages.sc>
2024-11-11 09:31:36 -0300

    Aula controllers

--- routes/web.js
+++ routes/web.js
@@ -1,65 +1,33 @@
 const express = require("express");
+const webProdutoController = require("../controllers_web/WebProdutoController");
+const webTipoProdutoController = require("../controllers_web/WebTipoProdutoController");
 const router = express.Router();
-const DataBase = require("../config/DataBase");
-const TipoProdutoModel = require("../models/TipoProdutoModel");
-const ProdutoModel = require("../models/ProdutoModel");
 
-router.get("/", (request, response) => {
-    response.render("index");
-});
+// Rotas de TipoProduto
+router.get("/tipoproduto", webTipoProdutoController.index);
+router.get("/tipoproduto/create", webTipoProdutoController.create);
+router.post("/tipoproduto", webTipoProdutoController.store);
+router.get("/tipoproduto/:tipoProdutoId", webTipoProdutoController.show);
+router.get("/tipoproduto/:tipoProdutoId/edit", webTipoProdutoController.edit);
+router.put("/tipoproduto/:tipoProdutoId", webTipoProdutoController.update);
+router.delete("/tipoproduto/:tipoProdutoId", webTipoProdutoController.destroy);
 
-router.get("/recurso", (request, response) => {
+// Rotas de Produto
+router.get("/produto", webProdutoController.index);
+router.get("/produto/create", webProdutoController.create);
+router.post("/produto", webProdutoController.store);
+router.get("/produto/:produtoId", webProdutoController.show);
+router.get("/produto/:produtoId/edit", webProdutoController.edit);
+router.put("/produto/:produtoId", webProdutoController.update);
+router.delete("/produto/:produtoId", webProdutoController.destroy);
+
+// Demais rotas ainda sem controlador (iremos criar um controlador para essas rotas no futuro)
+router.get("/recurso", async (request, response) => {
     response.render("Recurso/index");
 });
 
-router.get("/produto", async (request, response) => {
-    const produtos = await ProdutoModel.findAllWithTipoProdutoDescricao();
-    response.render("Produto/index", { produtos: produtos });
-});
-
-router.get("/produto/create", async (request, response) => {
-    const tipoProdutos = await DataBase.executeSQLQuery("SELECT * FROM TipoProduto");
-    response.render("Produto/create", { tipoProdutos: tipoProdutos });
-});
-
-router.post("/produto", async (request, response) => {
-    const numero = request.body.numero;
-    const nome = request.body.nome;
-    const preco = request.body.preco;
-    const TipoProduto_id = request.body.TipoProduto_id;
-    const ingredientes = request.body.ingredientes;
-    const timestamp = (new Date()).toISOString().slice(0, 19).replace('T', ' ');
-    const dataAtualizacao = timestamp;
-    const dataCriacao = timestamp;
-    const result = await DataBase.executeSQLQuery(`INSERT INTO Produto VALUES(null, ?, ?, ?, ?, ?, ?, ?)`,
-        [
-            numero,
-            nome,
-            preco,
-            TipoProduto_id,
-            ingredientes,
-            dataAtualizacao,
-            dataCriacao
-        ]
-    );
-    response.redirect("/produto");
-});
-
-
-router.get("/tipoproduto", async (request, response) => {
-    const tipoProdutos = await TipoProdutoModel.findAll();
-    response.render("TipoProduto/index", { tipoProdutos: tipoProdutos });
-});
-
-router.get("/tipoproduto/create", async (request, response) => {
-    response.render("TipoProduto/create");
-});
-
-router.post("/tipoproduto", async (request, response) => {
-    const tipoProduto = new TipoProdutoModel();
-    tipoProduto.descricao = request.body.descricao;
-    const result = await tipoProduto.save();
-    response.redirect("/tipoproduto");
+router.get("/", async (request, response) => {
+    response.render("index");
 });
 
 module.exports = router;
