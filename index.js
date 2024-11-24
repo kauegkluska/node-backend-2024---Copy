6bd8b9638cf45d9620c250464f19c066c62b901c
leobravoe <leonardo.bravo.estacio@gmail.com>
2024-11-24 19:32:52 -0300

    Aula 25/11/2024

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = require("./config/express");
 const port = app.get("port");
 
 app.listen(port, () => {
-    console.log(`Servidor rodando. http://127.0.0.1:${port}`);
+    console.log(`Servidor backend rodando. http://127.0.0.1:${port}`);
 });
