5ec963e6a83ec52852264a3a0541224893715f99
leobravoe <leonardo.bravo.estacio@gmail.com>
2024-11-25 09:46:31 -0300

    Completando o CRUD

--- config/express.js
+++ config/express.js
@@ -1,6 +1,9 @@
 // O objetivo desse arquivo é configurar o express
+
+const HbsConfigureCustomHelpers = require("./HbsConfigureCustomHelpers");
 const express = require("express");
 const config = require("config");
+const methodOverride = require("method-override");
 const app = express();
 const webRoutes = require("../routes/web");
 const apiRoutes = require("../routes/api");
@@ -14,6 +17,10 @@ app.set("view engine", "hbs");
 app.use(express.urlencoded({ extended: false }));
 // Middleware para criar rotas estáticas para todos os arquivos da pasta public
 app.use(express.static("./public"));
+// Configura os CustomHelpers do pacote hbs
+HbsConfigureCustomHelpers.run();
+// Configura o method-override no express para poder usar put ou delete nos <form> do HTML
+app.use(methodOverride("_method"));
 // Middleware - Utilizo um arquivo externo para definir as rotas WEB
 app.use(webRoutes);
 // Middleware - Utilizo um arquivo externo para definir as rotas API
--- controllers_web/WebProdutoController.js
+++ controllers_web/WebProdutoController.js
@@ -45,7 +45,8 @@ class WebProdutoController {
     * @param {Number} req.params.produtoId Parâmetro passado pela rota do express
     */
     async show(req, res) {
-        return res.send("show");
+        const produto = await ProdutoModel.findOneWithTipoProdutoDescricao(req.params.produtoId);
+        return res.render("Produto/show", { layout: "Layouts/main", title: "Show de Produto", produto: produto });
     }
 
     /**
@@ -55,9 +56,11 @@ class WebProdutoController {
     * @param {Number} req.params.produtoId Parâmetro passado pela rota do express
     */
     async edit(req, res) {
-
+        const produto = await ProdutoModel.findOne(req.params.produtoId);
+        const tipoProdutos = await TipoProdutoModel.findAll();
+        return res.render("Produto/edit", { layout: "Layouts/main", title: "Edit de Produto", tipoProdutos: tipoProdutos, produto: produto });
     }
-    
+
     /**
     * Atualiza um recurso existente no banco de dados
     * @param {*} req Requisição da rota do express
@@ -65,7 +68,16 @@ class WebProdutoController {
     * @param {Number} req.params.produtoId Parâmetro passado pela rota do express
     */
     async update(req, res) {
-
+        const produto = await ProdutoModel.findOne(req.params.produtoId);
+        if (produto) {
+            produto.numero = req.body.numero;
+            produto.nome = req.body.nome;
+            produto.preco = req.body.preco;
+            produto.TipoProduto_id = req.body.TipoProduto_id;
+            produto.ingredientes = req.body.ingredientes;
+            const result = await produto.update();
+        }
+        res.redirect("/produto");
     }
 
     /**
@@ -76,7 +88,7 @@ class WebProdutoController {
     */
     async destroy(req, res) {
         const produto = await ProdutoModel.findOne(req.params.produtoId);
-        if(produto){
+        if (produto) {
             await produto.delete();
         }
         return res.redirect(303, "/produto");
