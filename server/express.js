2d3db420bc04424557f5c432869ac2f8a3745f39
leobravoe <leonardo.bravo.estacio@gmail.com>
2025-02-03 18:25:57 -0300

    Organizando a arquitetura de pastas e adicionando novos arquivos

--- server/express.js
+++ server/express.js
@@ -14,22 +14,25 @@ app.set("port", process.env.PORT || config.get("server.port"));
 // Seto a template engine para renderizar views pelo método res.render
 app.set("view engine", "hbs");
 
-// Middleware do Express que é usado para fazer o parsing dos dados enviados pelo cliente através de formulários HTML
-app.use(express.urlencoded({ extended: false }));
-// Middleware para criar rotas estáticas para todos os arquivos da pasta public
-app.use(express.static("./public"));
 // Configura os CustomHelpers do pacote hbs
 HbsConfigureCustomHelpers.run();
 
-// Configura o middleware de session
+// Middleware - Usado para fazer o parsing dos dados enviados pelo cliente através de formulários HTML
+app.use(express.urlencoded({ extended: false }));
+// Middleware - Usado para criar rotas estáticas para todos os arquivos da pasta public
+app.use(express.static("./public"));
+// Middleware - Configura o method-override no express para poder usar put ou delete nos <form> do HTML
+app.use(methodOverride("_method"));
+// Middleware - Configura o middeware para fazer o parse no corpo da requisição e identificar dados no formato JSON
+app.use(express.json());
+
+// Middleware - Session
 app.use(session({
     secret: Crypto.randomBytes(32).toString('hex'), // chave secreta para assinar o cookie da session com 64 catacteres
     resave: false,
     saveUninitialized: true
 }));
 
-// Configura o method-override no express para poder usar put ou delete nos <form> do HTML
-app.use(methodOverride("_method"));
 // Middleware - Utilizo um arquivo externo para definir as rotas WEB
 app.use(webRoutes);
 // Middleware - Utilizo um arquivo externo para definir as rotas API
