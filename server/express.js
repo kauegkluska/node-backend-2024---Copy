b13fcbd028323e56a0c81839455e8079b17852b8
leobravoe <leonardo.bravo.estacio@gmail.com>
2025-02-03 18:15:23 -0300

    Organizando a arquitetura de pastas e adicionando novos arquivos

--- server/express.js
+++ server/express.js
@@ -13,6 +13,7 @@ const app = express();
 app.set("port", process.env.PORT || config.get("server.port"));
 // Seto a template engine para renderizar views pelo método res.render
 app.set("view engine", "hbs");
+
 // Middleware do Express que é usado para fazer o parsing dos dados enviados pelo cliente através de formulários HTML
 app.use(express.urlencoded({ extended: false }));
 // Middleware para criar rotas estáticas para todos os arquivos da pasta public
